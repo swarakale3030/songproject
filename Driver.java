30e1c975a25087cc7bd747e45ea9ed4f7075b582
swarakale3030 <swarakale3030@gmail.com>
2025-10-17 09:25:15 -0700

    Add files via upload
    latest update

--- Driver.java
+++ Driver.java
@@ -1,9 +1,9 @@
 public class Driver
 {
-    public static void main (String []args)
+    public static void main (String [] args)
     {
       ReadData r= new ReadData();
-      r.read();
+      Song[] song= r.read();
     }
 
 }
--- ReadData.java
+++ ReadData.java
@@ -7,37 +7,43 @@ public class ReadData{
 
     public void read () {
         Scanner in;
-        int count = 1;
+        int count = 0;
         Song[] song = new Song [28372];
         try
         {
              in = new Scanner(new File("songs 2025-2026 ANSIsc.csv"));
+             in.nextLine();
              while (in.hasNext())
              {
                 //artist_name;track_name;release_date;genre;len;shake the audience;obscene;danceability;loudness;topic
 
-                String title= in.nextLine().split(";")[1];
-                String artist=in.nextLine().split(";")[0];
-                String topic=in.nextLine().split(";")[9];
-                String genre=in.nextLine().split(";")[3];
-                double sta=Double.parseDouble(in.nextLine().split(";")[5]);
-                double obscene=Double.parseDouble(in.nextLine().split(";")[6]);
-                double loudness=Double.parseDouble(in.nextLine().split(";")[8]);
-                int year=Integer.parseInt(in.nextLine().split(";")[2]);
-                String danceability=in.nextLine().split(";")[7];
-                double len=Double.parseDouble(in.nextLine().split(";")[4]);
+                String line = in.nextLine();
+                String title = line.split(";")[1];
+                String artist = line.split(";")[0];
+                String topic = line.split(";")[9];
+                String genre =line.split(";")[3];
+                double sta = Double.parseDouble(line.split(";")[5]);
+                double obscene = Double.parseDouble(line.split(";")[6]);
+                double loudness = Double.parseDouble(line.split(";")[8]);
+                int year = Integer.parseInt(line.split(";")[2]);
+                double danceability = Double.parseDouble(line.split(";")[7]);
+                double len = Double.parseDouble(line.split(";")[4]);
                 System.out.print(count+ ":");
-                System.out.println(in.nextLine());
+               System.out.println(line);
                 
                 count++;
-                Song[]=(String artist, String title, int year, String genre, double len, double sta, double obscene, double danceability, double loudness, String topic)
-             }
+                song[count] = new Song (artist, title, year,  genre,  len,  sta,  obscene,  danceability,  loudness,  topic);}
              in.close();
         }
-        catch ( IOException e){
+        catch ( IOException e)
+        {
             System.out.println("error in scanner");
-            return;
+            return song;
         }
+
+        
+
+        
     
 
 
